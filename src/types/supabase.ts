d846233e254ff963adffed1a05fec5869a3eaf2d
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-07-15 14:58:18 +0500

    feat: Supabase DB

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -1,1 +1,26 @@
-export type Database = object;
+export type Database = {
+  public: {
+    Tables: {
+      registered_users: {
+        Row: {
+          id: number;
+          created_at: string; 
+          email: string | null;
+          approved: boolean | null;
+        };
+        Insert: {
+          email?: string | null;
+          approved?: boolean | null;
+          created_at?: string;
+          id?: number;
+        };
+        Update: {
+          email?: string | null;
+          approved?: boolean | null;
+          created_at?: string;
+          id?: number;
+        };
+      };
+    };
+  };
+};
