da07c4ef34b511a6d60fffb49f50644a9d5febc6
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-07-16 13:13:57 +0500

    feat: single instance Browser Client createad

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,11 @@
 import { createBrowserClient } from '@supabase/ssr';
-
 import { env } from '@/env';
-
 import { Database } from '@/types/supabase';
+
+export const supabase = createBrowserClient<Database>(
+  env.NEXT_PUBLIC_SUPABASE_URL,
+  env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+);
 
 export function createSupabaseBrowserClient() {
   return createBrowserClient<Database>(
