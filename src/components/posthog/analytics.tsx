d683dadbc83a71c17ce41137518f233629c627a1
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-07-18 01:03:26 -0700

    refactor: removed posthog

--- src/components/posthog/analytics.tsx
+++ src/components/posthog/analytics.tsx
@@ -1,20 +1,20 @@
-import React, { Suspense } from 'react';
+// import React, { Suspense } from 'react';
 
-import { env } from '@/env';
+// import { env } from '@/env';
 
-import IdentifyUser from './identify';
-import PageView from './page-view';
+// import IdentifyUser from './identify';
+// import PageView from './page-view';
 
-const PosthogAnalytics = () => {
-  if (!env.NEXT_PUBLIC_POSTHOG_HOST) return null;
-  return (
-    <>
-      <IdentifyUser />
-      <Suspense>
-        <PageView />
-      </Suspense>
-    </>
-  );
-};
+// const PosthogAnalytics = () => {
+//   if (!env.NEXT_PUBLIC_POSTHOG_HOST) return null;
+//   return (
+//     <>
+//       <IdentifyUser />
+//       <Suspense>
+//         <PageView />
+//       </Suspense>
+//     </>
+//   );
+// };
 
-export default PosthogAnalytics;
+// export default PosthogAnalytics;
--- src/components/posthog/identify.ts
+++ src/components/posthog/identify.ts
@@ -1,47 +1,47 @@
-'use client';
+// 'use client';
 
-import { usePostHog } from 'posthog-js/react';
-import { useEffect, useState } from 'react';
+// import { usePostHog } from 'posthog-js/react';
+// import { useEffect, useState } from 'react';
 
-import { createSupabaseBrowserClient } from '@/lib/supabase/client';
+// import { createSupabaseBrowserClient } from '@/lib/supabase/client';
 
-import { PosthogEventKeys } from '@/constants/posthog-events';
+// import { PosthogEventKeys } from '@/constants/posthog-events';
 
-type TUserIdentify = {
-  email: string | undefined;
-  id: string;
-};
+// type TUserIdentify = {
+//   email: string | undefined;
+//   id: string;
+// };
 
-export default function Identify() {
-  const posthog = usePostHog();
-  const supabase = createSupabaseBrowserClient();
-  const [user, setUser] = useState<TUserIdentify | null>(null);
+// export default function Identify() {
+//   const posthog = usePostHog();
+//   const supabase = createSupabaseBrowserClient();
+//   const [user, setUser] = useState<TUserIdentify | null>(null);
 
-  // subscribe to supabase auth changes
-  useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === 'SIGNED_IN' && session?.user) {
-        setUser({
-          email: session.user.email,
-          id: session.user.id,
-        });
-      } else if (event === 'SIGNED_OUT') {
-        posthog.reset();
-        window.location.replace('/auth/sign-in');
-      }
-    });
+//   // subscribe to supabase auth changes
+//   useEffect(() => {
+//     const { data } = supabase.auth.onAuthStateChange((event, session) => {
+//       if (event === 'SIGNED_IN' && session?.user) {
+//         setUser({
+//           email: session.user.email,
+//           id: session.user.id,
+//         });
+//       } else if (event === 'SIGNED_OUT') {
+//         posthog.reset();
+//         window.location.replace('/auth/sign-in');
+//       }
+//     });
 
-    return () => {
-      data.subscription.unsubscribe();
-    };
-  }, [posthog, supabase]);
+//     return () => {
+//       data.subscription.unsubscribe();
+//     };
+//   }, [posthog, supabase]);
 
-  useEffect(() => {
-    if (user?.id && user?.email) {
-      posthog.identify(user.id, { email: user.email });
-      posthog.capture(PosthogEventKeys.LOGIN, { email: user.email });
-    }
-  }, [user?.id, user?.email, posthog]);
+//   useEffect(() => {
+//     if (user?.id && user?.email) {
+//       posthog.identify(user.id, { email: user.email });
+//       posthog.capture(PosthogEventKeys.LOGIN, { email: user.email });
+//     }
+//   }, [user?.id, user?.email, posthog]);
 
-  return null;
-}
+//   return null;
+// }
