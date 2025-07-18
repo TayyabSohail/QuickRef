75f334cbca4782b42108b91512004fd5c4efdb00
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-07-18 12:27:11 -0700

    fix signup bug

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { LoginForm } from '@/components/auth/login-form';
 import { GridSmallBackground } from '@/components/ui/GridSmallBackgroundDemo';
 import { Suspense } from 'react';
