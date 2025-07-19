23aef0b4db3b6b3824caf969d6add87165dd23f6
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-07-18 23:41:19 -0700

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,6 @@ import { FeatureGrid } from '@/components/landing/FeatureGrid';
 import { CallToAction } from '@/components/landing/CallToAction';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    const code = url.searchParams.get('code');
-    const type = url.searchParams.get('type') || 'signup';
-
-    if (code) {
-      router.replace(`/auth/login?code=${code}&type=${type}`);
-    }
-  }, [router]);
-
   return (
     <div>
       <Hero />
